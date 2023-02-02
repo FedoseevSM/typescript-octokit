fb4dcfcb9ab77f9972b09f8d8503bc34c7a7d6a7
FedoseevSM <76589678+FedoseevSM@users.noreply.github.com>
2023-02-02 22:38:57 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -7,15 +7,26 @@ const octokit = new Octokit({
 })
 
 function createForkInOrganization() {
-  const repo = octokit.rest.repos.createFork({
+  octokit.rest.repos.createFork({
     owner: "FedoseevSM",
     repo: "spa-template-vue-bootstrap-express-prisma",
-    organization: "forkpack"
+    organization: "forkpack",
+    name: "same-fork-repo-new-name2"
   }).then((response => {
     console.log("repo forked to organization")
   }))
 }
 // createForkInOrganization()
+
+function createRepoInOrganization() {
+  octokit.rest.repos.createInOrg({
+    org: "forkpack",
+    name: "test-repo",
+  }).then((response => {
+    console.log("create repo in organization")
+  }))
+}
+// createRepoInOrganization()
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
